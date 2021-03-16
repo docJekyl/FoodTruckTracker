e29c7bb59c2364ca6c86205ed90373c45c20affb
Eric Hyde <hydestuf@gmail.com>
2021-03-16 16:24:00 -0500

    added nav bar, connected to state on index.js

---
+++ client/components/customer/customer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Customer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return(
+      <div>Hello from Customer</div>
+    )
+  }
+}
+
+export default Customer;
--- client/index.jsx
+++ client/index.jsx
@@ -1,31 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Container, Button, Row, Col } from 'react-bootstrap';
+import Header from './components/header.jsx'
+import Customer from './components/customer/customer.jsx';
+import Vendor from './components/vendor/Vendor.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: ''
+      nav: '',
+      user: null,
+      userLocation: null,
+      foodTruckData: null
     }
     this.location = this.location.bind(this);
+    this.setNavState = this.setNavState.bind(this);
+  }
+
+  setNavState(name) {
+    this.setState({nav: name});
+    console.log(this.state.nav)
   }
 
   location() {
-    // if ('geolocation' in navigator) {
-    //   console.log('geolocator found')
-    //   var loc = getCurrentPosition();
-    //   console.log(loc);
-    // } else {
-    //   console.log('geolocation not available')
-    // }
+    if ('geolocation' in navigator) {
+      console.log('geolocator found')
+      var loc = getCurrentPosition();
+      console.log(loc);
+    } else {
+      console.log('geolocation not available')
+    }
   }
+
 
 
   render() {
     return(
       <div>
+        <Header fn={this.setNavState}/>
         <h1>Hello from the FTTracker App!</h1>
+        <Container>
+          <Row>
+            <Col>
+              <Button>I'm a Vendor!</Button>
+            </Col>
+            <Col>
+              <Button>I'm Hungry!</Button>
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   }
